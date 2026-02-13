38f8a4e3bbd2298eb0bf74162f8127853cd3e970
Виталий <saifutdinov_vitalii@mail.ru>
2026-02-13 16:33:43 +0500

    dfsf

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     //гамбургер
-     const hamburger = document.querySelector('.hamburger');
-     const header = document.querySelector('.header')
+    const hamburger = document.querySelector('.hamburger');
+    const header = document.querySelector('.header')
     hamburger.addEventListener('click', ()=> {
         header.classList.toggle('open-menu');
+    });
+    //Добавляем появления стрелки вверх
+    const top = document.querySelector('.top');
+    const main = document.querySelector('.main');
+    let mainOffsetY = main.offsetHeight;
+    let headerOffsetY = header.offsetHeight;
+
+    window.addEventListener('scroll', ()=> {
+        if(window.scrollY > headerOffsetY) {
+            header.style.position = 'fixed';
+            header.style.top = `-100px`;            
+        }
+        if(window.scrollY > mainOffsetY) {
+            header.style.position = 'fixed';
+            header.style.top = `0px`;  
+            top.style.display = 'flex';
+        } else if (window.scrollY > mainOffsetY - `100px`) {
+            header.style.position = 'relative';
+            header.style.top = `0`; 
+        } else {
+            top.style.display = 'none';          
+            header.style.position = 'relative';
+            header.style.top = `0`;  
+        }
+        
+        
     })
 })
